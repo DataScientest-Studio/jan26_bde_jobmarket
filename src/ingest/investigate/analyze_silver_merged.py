6dfc64aca8f8169056332f699daf845ac47b880c
William Ramarques <willramarques@gmail.com>
2026-03-11 18:35:21 +0100

    Print stat of silver merge dataset

--- src/ingest/investigate/analyze_silver_merged.py
+++ src/ingest/investigate/analyze_silver_merged.py
@@ -2,13 +2,13 @@ from src.config.env import load_project_env
 load_project_env()  # safe à rappeler (idempotent)
 
 from src.storage.storage import get_storage_from_env
-import src.utils.merge_dataset_utils as merge_utils
+import src.utils.merge_dataset_utils as merge_dataset_utils
 import logging
 logger = logging.getLogger(__name__)
 
 storage_wttj = get_storage_from_env("silver", "merged")
-df = merge_utils.read_wttj_parquet_file_to_df(storage_wttj,"")
+df = merge_dataset_utils.read_wttj_parquet_file_to_df(storage_wttj,"")
 if df is not None and not df.empty:
-    merge_utils.print_statistics(df)
+    merge_dataset_utils.print_statistics(df)
 else    :
     logger.warning("⚠️ Aucune donnée chargée pour les statistiques FT/WTTJ fusionnées")
